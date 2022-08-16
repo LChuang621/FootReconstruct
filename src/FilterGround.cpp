9a3046d4fd537bb757cd14b652b34eccafa44671
LChuang621 <346722249@qq.com>
2022-08-16 10:21:03 +0800

    切分地面

--- src/FilterGround.cpp
+++ src/FilterGround.cpp
@@ -10,6 +10,7 @@ using std::endl;
 int main()
 {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
     std::string pointcloud = "D:/work/reconstruct/data/origin_fullcloud/cloud_0815.ply";
 
     cout << pointcloud << endl;
@@ -22,6 +23,56 @@ int main()
 
 	cout << "文件共有" << cloud->size() << "个点：" << endl;
 
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+
+	// Create the segmentation object
+	pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+
+	// Optional
+	seg.setOptimizeCoefficients(true);
+	// Mandatory
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	// 距离阈值 单位m
+	seg.setDistanceThreshold(8);
+	seg.setInputCloud(cloud);
+	seg.segment(*inliers, *coefficients);
+	if (inliers->indices.size() == 0)
+	{
+		PCL_ERROR("Could not estimate a planar model for the given dataset.");
+		return (-1);
+	}
+
+	// 提取地面
+	pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+	extract.setInputCloud(cloud);
+	extract.setIndices(inliers);
+	extract.filter(*cloud_filtered);
+
+	pcl::PCDWriter writer;
+	writer.write<pcl::PointXYZRGB>("3dpoints_ground.pcd", *cloud_filtered, false);
+#if 0
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+	viewer->setBackgroundColor(0, 0, 0);
+	viewer->addPointCloud<pcl::PointXYZRGB>(cloud_filtered, "sample cloud");
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+	viewer->addCoordinateSystem(1.0);
+	viewer->initCameraParameters();
+
+	while (!viewer->wasStopped())
+	{
+		viewer->spinOnce(100);
+		//boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+	}
+#endif
+
+
+	// 提取除地面外的物体
+	extract.setNegative(true);
+	extract.filter(*cloud_filtered);
+
+	writer.write<pcl::PointXYZRGB>("3dpoints_object.pcd", *cloud_filtered, false);
 
     return 0;
 }
--- src/FilterGround.h
+++ src/FilterGround.h
@@ -3,3 +3,13 @@
 
 #include <pcl/point_types.h>
 #include <pcl/io/ply_io.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/PointIndices.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/io/pcd_io.h>
+
+
+#include <pcl/visualization/pcl_visualizer.h>
+
+
